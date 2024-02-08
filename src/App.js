b8ea0d7efef39e392b96abac5d5cf093c5b9ae47
proskillowner <116067530+proskillowner@users.noreply.github.com>
2024-02-09 08:30:36 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,211 @@
+import { useState } from 'react';
+import './App.css';
+import * as satsConnect from 'sats-connect';
+import * as psbt from "./psbt.ts";
+
+const NETWORK = 'Mainnet'
+const RECIPIENT_ADDRESS = 'bc1qeaacuf2d7anrdrcez02mn0lh2zfhscy9c3f0na'
+const INSCRIPTION_ID = '6db9dd785874a07a526acda73aa95ee45b117adfba7d7e5d454236984c4b0b79i0'
+const FEE_RATE = 23
+
+export const App = () => {
+  const [walletConnected, setWalletConnected] = useState(false)
+  const [paymentAccount, setPaymentAccount] = useState(null);
+  const [ordinalsAccount, setOrdinalsAccount] = useState(null);
+
+  const setAccounts = (accounts) => {
+    if (accounts) {
+      setWalletConnected(true)
+      for (const account of accounts) {
+        if (account.purpose === 'payment') {
+          setPaymentAccount(account)
+        } else if (account.purpose === 'ordinals') {
+          setOrdinalsAccount(account)
+        }
+      }
+    } else {
+      setWalletConnected(false)
+      setPaymentAccount(null)
+      setOrdinalsAccount(null)
+    }
+  }
+
+  const connectWallet = async () => {
+    const getAddressOptions = {
+      payload: {
+        network: {
+          type: NETWORK,
+        },
+        purposes: ['payment', 'ordinals'],
+        message: 'Connect Wallet'
+      },
+      onFinish: (response) => {
+        setWalletConnected(true)
+        setAccounts(response.addresses)
+      },
+      onCancel: () => {
+      },
+    }
+
+    await satsConnect.getAddress(getAddressOptions);
+  }
+
+  const signPsbt = async (payment, ordinals, recipientAddress, feeRate) => {
+    try {
+      const unsignedPsbt = await psbt.generatePsbt(
+        payment,
+        ordinals,
+        recipientAddress,
+        feeRate,
+      )
+
+      if (!unsignedPsbt) {
+        console.error('Invalid PSBT')
+        return
+      }
+
+      const inputsToSign = []
+
+      if (ordinals) {
+        inputsToSign.push({
+          address: ordinals.address,
+          signingIndexes: [0],
+          sigHash: 0,
+        })
+      }
+
+      for (let i = 0; i < unsignedPsbt.paymentUtxoCount; i++) {
+        inputsToSign.push({
+          address: payment.address,
+          signingIndexes: [i + ordinals ? 1 : 0],
+          sigHash: 0,
+        })
+      }
+
+      let psbtBase64
+      let txid
+
+      const signPsbtOptions = {
+        payload: {
+          network: {
+            type: NETWORK,
+          },
+          message: 'Sign Transaction',
+          psbtBase64: unsignedPsbt.psbtBase64,
+          broadcast: false,
+          inputsToSign,
+        },
+        onFinish: (response) => {
+          psbtBase64 = response.psbtBase64
+          txid = response.txId
+        },
+        onCancel: () => {
+        },
+      }
+
+      await satsConnect.signTransaction(signPsbtOptions)
+
+      return txid
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const sendPayment = async () => {
+    try {
+      const payment = {
+        addressType: paymentAccount.addressType,
+        address: paymentAccount.address,
+        publicKey: paymentAccount.publicKey,
+        amount: 100000,
+      }
+
+      const txid = await signPsbt(payment, null, RECIPIENT_ADDRESS, FEE_RATE)
+
+      if (txid) {
+        console.log(txid)
+      } else {
+        alert('Error')
+      }
+    } catch (error) {
+      console.error(JSON.stringify(error))
+      alert(JSON.stringify(error))
+    }
+  }
+
+  const sendOrdinals = async () => {
+    try {
+      const payment = {
+        addressType: paymentAccount.addressType,
+        address: paymentAccount.address,
+        publicKey: paymentAccount.publicKey,
+        amount: 0,
+      }
+
+      const ordinals = {
+        addressType: ordinalsAccount.addressType,
+        address: ordinalsAccount.address,
+        publicKey: ordinalsAccount.publicKey,
+        inscriptionId: INSCRIPTION_ID,
+      }
+
+      const txid = await signPsbt(payment, ordinals, RECIPIENT_ADDRESS, FEE_RATE)
+
+      if (txid) {
+        console.log(txid)
+      } else {
+        alert('Error')
+      }
+    } catch (error) {
+      console.error(JSON.stringify(error))
+      alert(JSON.stringify(error))
+    }
+  }
+
+  const signMessage = async () => {
+    try {
+      const signMessageOptions = {
+        payload: {
+          network: {
+            type: NETWORK,
+          },
+          address: paymentAccount.address,
+          message: "Hello World",
+        },
+        onFinish: (signature) => {
+          console.log(signature)
+        },
+        onCancel: () => { }
+      };
+      await satsConnect.signMessage(signMessageOptions);
+    } catch (error) {
+      console.log(JSON.stringify(error))
+      alert(JSON.stringify(error))
+    }
+  }
+
+  return (
+    <>
+      {(!walletConnected) && (<button onClick={connectWallet}>Connect Wallet</button>)}
+      {(walletConnected) && (
+        <>
+          <>Payment</><br />
+          <>Address: {paymentAccount.address}</><br />
+          <>Public Key: {paymentAccount.publicKey}</><br />
+          <>Address Type: {paymentAccount.addressType}</><br />
+          <>Purpose: {paymentAccount.purpose}</><br />
+          <br />
+          <>Ordinals</><br />
+          <>Address: {ordinalsAccount.address}</><br />
+          <>Public Key: {ordinalsAccount.publicKey}</><br />
+          <>Address Type: {ordinalsAccount.addressType}</><br />
+          <>Purpose: {ordinalsAccount.purpose}</><br />
+          <br />
+          <button onClick={sendPayment} >Send Payment</button>
+          <button onClick={sendOrdinals} >Send Ordinals</button>
+          <button onClick={signMessage} >Sign Message</button>
+        </>
+      )}
+    </>
+  );
+}
---
+++ src/psbt.ts
@@ -0,0 +1,239 @@
+import { base64, hex } from '@scure/base'
+import * as btc from '@scure/btc-signer'
+import * as secp256k1 from '@noble/secp256k1'
+import axios from 'axios'
+
+const SERVER_URL = 'https://ordiswap-api.proskillowner.com'
+
+const NETWORK = btc.NETWORK
+const MIN_RELAY_FEE = 1000
+
+const DUMMY_PRIVATEKEY = '0000000000000000000000000000000000000000000000000000000000000001'
+const dummyPublicKey = secp256k1.getPublicKey(DUMMY_PRIVATEKEY, true).slice(1)
+
+export const ADDRESS_TYPE_P2SH = 'p2sh'
+export const ADDRESS_TYPE_P2PKH = 'p2pkh'
+export const ADDRESS_TYPE_P2SH_P2WPKH = 'p2sh_p2wpkh'
+export const ADDRESS_TYPE_P2WPKH = 'p2wpkh'
+export const ADDRESS_TYPE_P2TR = 'p2tr'
+
+const getInputInfo = (addressType: string, publicKey: Uint8Array, txid: string, vout: number, amount: string) => {
+    let input: {
+        txid: string
+        index: number
+        witnessUtxo?: any
+        redeemScript?: any
+        tapInternalKey?: Uint8Array
+    } = {
+        txid,
+        index: vout,
+    }
+
+    if (addressType === ADDRESS_TYPE_P2SH) {
+        const p2pkh = btc.p2pkh(publicKey, NETWORK)
+        const p2sh = btc.p2sh(p2pkh, NETWORK)
+
+        input.redeemScript = p2sh.redeemScript
+        input.witnessUtxo = {
+            script: p2sh.script,
+            amount: BigInt(amount),
+        }
+    } else if (addressType === ADDRESS_TYPE_P2PKH) {
+        const p2pkh = btc.p2pkh(publicKey, NETWORK)
+
+        input.witnessUtxo = {
+            script: p2pkh.script,
+            amount: BigInt(amount),
+        }
+    } else if (addressType === ADDRESS_TYPE_P2SH_P2WPKH) {
+        const p2wpkh = btc.p2wpkh(publicKey, NETWORK)
+        const p2sh = btc.p2sh(p2wpkh, NETWORK)
+
+        input.redeemScript = p2sh.redeemScript
+        input.witnessUtxo = {
+            script: p2sh.script,
+            amount: BigInt(amount),
+        }
+    } else if (addressType === ADDRESS_TYPE_P2WPKH) {
+        const p2wpkh = btc.p2wpkh(publicKey, NETWORK)
+
+        input.witnessUtxo = {
+            script: p2wpkh.script,
+            amount: BigInt(amount),
+        }
+    } else if (addressType === ADDRESS_TYPE_P2TR) {
+        const p2tr = btc.p2tr(publicKey, undefined, NETWORK)
+
+        input.tapInternalKey = publicKey
+        input.witnessUtxo = {
+            script: p2tr.script,
+            amount: BigInt(amount),
+        }
+    }
+
+    return input
+}
+
+export const generatePsbt = async (
+    payment: any,
+    ordinals: any,
+    recipientAddress: string,
+    feeRate: number,
+) => {
+    try {
+        const tx = new btc.Transaction()
+        const dummyTx = new btc.Transaction()
+
+        let totalUtxoValue = 0
+
+        if (ordinals) {
+            let resp = await axios.get(`${SERVER_URL}/getInscriptionUtxoList?address=${ordinals.address}`)
+
+            if (!resp || resp.status !== 200 || !resp.data) {
+                console.error('No ordinals UTXO exist')
+                return
+            }
+    
+            let ordinalsUtxos = resp.data.data
+            ordinalsUtxos = ordinalsUtxos.filter((utxo: any) => !utxo.isSpent)
+
+            const ordinalsUtxo = ordinalsUtxos.find((ordinalsUtxo: any) => {
+                return ordinalsUtxo.inscriptions.find((inscription: any) => inscription.inscriptionId === ordinals.inscriptionId)
+            })
+
+            if (!ordinalsUtxo) {
+                console.error('Ordinals UTXO not exist')
+                return
+            }
+
+            let ordinalsPublicKey = hex.decode(ordinals.publicKey)
+            if (ordinalsPublicKey.length === 33) {
+                ordinalsPublicKey = ordinalsPublicKey.slice(1)
+            }
+
+            const ordinalsInput = getInputInfo(
+                ordinals.addressType,
+                ordinalsPublicKey,
+                ordinalsUtxo.txid,
+                ordinalsUtxo.vout,
+                ordinalsUtxo.amount,
+            )
+
+            const dummyOrdinalsInput = getInputInfo(
+                ordinals.addressType,
+                dummyPublicKey,
+                ordinalsUtxo.txid,
+                ordinalsUtxo.vout,
+                ordinalsUtxo.amount,
+            )
+
+            payment.amount += ordinalsUtxo.amount
+            totalUtxoValue += ordinalsUtxo.amount
+
+            tx.addInput(ordinalsInput)
+
+            dummyTx.addInput(dummyOrdinalsInput)
+        }
+
+        if (!payment.amount) {
+            console.error('Empty output')
+            return
+        }
+
+        tx.addOutputAddress(recipientAddress, BigInt(payment.amount), NETWORK)
+
+        dummyTx.addOutputAddress(recipientAddress, BigInt(payment.amount), NETWORK)
+
+        let resp = await axios.get(`${SERVER_URL}/getBtcUtxoList?address=${payment.address}`)
+
+        if (!resp || resp.status !== 200 || !resp.data) {
+            console.error('No payment UTXO exist')
+            return
+        }
+
+        let paymentUtxos = resp.data.data
+        paymentUtxos = paymentUtxos.filter((utxo: any) => !utxo.isSpent)
+        paymentUtxos = paymentUtxos.sort((a: any, b: any) => b.amount - a.amount)
+        let paymentUtxoCount = 0
+
+        let paymentPublicKey = hex.decode(payment.publicKey)
+        if (paymentPublicKey.length === 33) {
+            paymentPublicKey = paymentPublicKey.slice(1)
+        }
+
+        for (const paymentUtxo of paymentUtxos) {
+            const paymentInput = getInputInfo(
+                payment.addressType,
+                paymentPublicKey,
+                paymentUtxo.txid,
+                paymentUtxo.vout,
+                paymentUtxo.amount
+            )
+
+            const dummyPaymentInput = getInputInfo(
+                payment.addressType,
+                dummyPublicKey,
+                paymentUtxo.txid,
+                paymentUtxo.vout,
+                paymentUtxo.amount
+            )
+
+            tx.addInput(paymentInput)
+
+            dummyTx.addInput(dummyPaymentInput)
+
+            paymentUtxoCount++
+
+            let feeTx = btc.Transaction.fromPSBT(dummyTx.toPSBT())
+            feeTx.sign(hex.decode(DUMMY_PRIVATEKEY))
+            feeTx.finalize()
+
+            let feeAmount = feeTx.vsize * feeRate
+            feeAmount = feeAmount < MIN_RELAY_FEE ? MIN_RELAY_FEE : feeAmount
+
+            totalUtxoValue += paymentUtxo.amount
+            if (totalUtxoValue >= payment.amount + feeAmount) {
+                dummyTx.addOutputAddress(payment.address, BigInt(feeAmount), NETWORK)
+
+                feeTx = btc.Transaction.fromPSBT(dummyTx.toPSBT())
+                feeTx.sign(hex.decode(DUMMY_PRIVATEKEY))
+                feeTx.finalize()
+
+                feeAmount = feeTx.vsize * feeRate
+                feeAmount = feeAmount < MIN_RELAY_FEE ? MIN_RELAY_FEE : feeAmount
+
+                if (totalUtxoValue >= payment.amount + 100 * feeRate + feeAmount) {
+                    tx.addOutputAddress(payment.address, BigInt(totalUtxoValue - payment.amount - feeAmount), NETWORK)
+                }
+
+                const psbt = tx.toPSBT()
+                const psbtBase64 = base64.encode(psbt)
+
+                return {
+                    psbt,
+                    psbtBase64,
+                    paymentUtxoCount,
+                }
+            }
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const pushPsbt = async (psbt: any) => {
+    try {
+        const tx = btc.Transaction.fromPSBT(psbt)
+        tx.finalize()
+
+        const response = await axios.post(`${SERVER_URL}/sendRawTransaction`, {
+            rawTransaction: tx.hex
+        })
+
+        if (response && response.status === 200 && response.data) {
+            return response.data.data
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
